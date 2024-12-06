07223df9640fa035b3ea77081576b12b0342d899
jacks <jackson.krajeski@gmail.com>
2024-12-05 23:46:14 -0500

    Changes

--- src/main/java/net/jacks/tutorialmod/entity/custom/SnowLeopardEntity.java
+++ src/main/java/net/jacks/tutorialmod/entity/custom/SnowLeopardEntity.java
@@ -60,10 +60,10 @@ public class SnowLeopardEntity extends Animal {
 
     public static AttributeSupplier.Builder createAttributes(){
         return Animal.createLivingAttributes()
-                .add(Attributes.MAX_HEALTH,20D)
-                .add(Attributes.MOVEMENT_SPEED,.25D)
-                .add(Attributes.FOLLOW_RANGE, 15D)
-                .add(Attributes.ATTACK_DAMAGE, 2f)
+                .add(Attributes.MAX_HEALTH,15D)
+                .add(Attributes.MOVEMENT_SPEED,.3D)
+                .add(Attributes.FOLLOW_RANGE, 10D)
+                .add(Attributes.ATTACK_DAMAGE, 6f)
                 .add(Attributes.ATTACK_SPEED, 1.5f);
 
     }
